e6b0c808f351eec35ad8d52f2b9fb5042c86dcd9
khushboo <khushboobhatoia@gmail.com>
2018-03-13 11:55:42 -0400

    Add files via upload

---
+++ ConditionStatments/src/conditionstatments/ConditionStatments.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conditionstatments;
+
+/**
+ *
+ * @author macstudent
+ */
+public class ConditionStatments {
+
+    public static void main(String[] args) {
+      /*  int number = 20;
+       if(number > 10)
+       {
+         System.out.println("MORE THAN 10");
+       }
+       else if(number == 10)
+               {
+                   System.out.println("EQUAL TO 10");
+               }
+       
+       switch(number)
+       {
+           case 10:
+               System.out.println("VALUE = 10");
+               break;
+               case 20:
+               System.out.println("VALUE = 20");
+               break;
+               case 30:
+               System.out.println("VALUE = 30");
+               break;
+               default:
+               System.out.println("NO MATCHING VALUE");
+               break;
+       }
+       
+       
+       char vowel = 'a';
+       switch(vowel)
+       {
+           case 'a':
+               System.out.println("VOWEL");
+               break;
+               case 'e':
+               System.out.println("VOWEL");
+               break;
+               case 'i':
+               System.out.println("VOWEL");
+               break;
+               case 'o':
+               System.out.println("VOWEL");
+               break;
+               case 'u':
+               System.out.println("VOWEL");
+               break;
+               default:
+               System.out.println("NOT A VOWEL");
+               break;
+               
+       }
+       
+       String province = "ALBERTA";
+       switch(province)
+       {
+           case "Ontario":
+               System.out.println("ON");
+               break;
+           case "Alberta":
+               System.out.println("AB");
+               break;
+           case "Prince Edward":
+               System.out.println("PE");
+               break;
+           default:
+               System.out.println("UNAVAILABLE");
+               break;
+       }
+               
+       int numbers[] = new int[5];
+       
+       int i;
+       
+       for(i=0;i<=numbers.length;i++)
+       {
+           //number[i] = 10;     error
+           numbers[i] = (int)(Math.random()*100);
+           System.out.println("NUMBERS [" + i + "] =" +numbers[i]);
+       }
+       double PI = Math.PI;
+       double power = Math.pow(2,2);
+       Math.sqrt(144);
+       Math.abs(PI.VALUE);
+       */
+
+      
+      
+      
+    }
+    
+}
---
+++ ConditionStatments/src/conditionstatments/Pattern1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conditionstatments;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Pattern1 {
+    public static void main(String[] args) {
+    int n = 5;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n;j++)
+      {
+      if((j==1 || j==n) || (i==1 || i==n))
+      {
+         System.out.print(" *");//1 space
+      }else
+      {
+          System.out.print("  ");//2 spaces
+      }
+      }
+      System.out.println();
+    }
+}
+    
+}
